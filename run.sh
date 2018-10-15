29374a985fe33d432c08c36682fc138cf4713421
Chris Tinning <chris.tinning@qikserve.com>
2018-10-15 09:09:28 +0100

    Improve use of docker-compose and add progress

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 ./gradlew bootJar
-docker-compose build
-docker-compose up
+docker-compose up --build -d
+
+progress="..."
+until curl -s http://localhost:8080/actuator/health 1> /dev/null
+do 
+    echo -ne "Waiting on service to start${progress}\r"
+    progress="${progress}."
+    sleep 1; 
+done
+echo ""
